bce365ccc723497ffc41ffeb670bacc9d0336bfb
dmytroCoder <dmytrovy2012@gmail.com>
2026-07-05 19:06:57 +0200

    Add files via upload

---
+++ VIP-Sweden-Bygg-starterpage/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
